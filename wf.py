411cda14e3d0639405d370d683b60646addb71cd
Joshua Whittaker <whittaker.d.joshua@gmail.com>
2022-09-27 02:43:07 +0200

    Testing some stuff

--- wf.py
+++ wf.py
@@ -3,7 +3,7 @@ import argparse
 
 WFAPI_DROPS_ENDPOINT = 'https://api.warframestat.us/drops/search/'
 EXCLUSIONS = ['Exceptional', 'Flawless', 'Radiant']
-MEME = "Bronco Prime Blueprint"
+MEME = "Bronco Prime Blueprint".split()
 
 def get_drops(query):
     url = WFAPI_DROPS_ENDPOINT + query
@@ -16,7 +16,7 @@ def print_filtered_results(results):
             print(f"Place: {drop['place']}\nItem: {drop['item']}\nRarity: {drop['rarity']}\nChance: {drop['chance']}%\n")
 
 parser = argparse.ArgumentParser(description="Warframe CLI helper tool to find drop locations for items.")
-parser.add_argument('query', nargs='+', default=MEME, help="The item for which you would like to seach.")
+parser.add_argument('query', nargs='*', default=MEME, help="The item for which you would like to seach.")
 args = parser.parse_args()
 
 query = " ".join(args.query)
